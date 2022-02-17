e2e7ba4bd3a8643d6dcefca9c90d414e0cfc8ace
Jinki Lee <jinkilee73@gmail.com>
2022-02-17 12:30:53 +0900

    add virtualbox installation

--- script/install-minikube.sh
+++ script/install-minikube.sh
@@ -2,3 +2,10 @@
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+
+# install virtual-box
+sudo apt install virtualbox virtualbox-ext-pack
+
+
+
